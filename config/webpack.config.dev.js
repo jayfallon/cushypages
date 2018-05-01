37df9467b73ff52ac21fe24700af08c9e48b9f59
Jay Fallon <jayfallon@gmail.com>
2018-05-01 12:24:50 -0400

    added sass loader and configured webpack dev

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -1,5 +1,3 @@
-
-
 const autoprefixer = require('autoprefixer');
 const path = require('path');
 const webpack = require('webpack');
@@ -152,6 +150,43 @@ module.exports = {
 					// "style" loader turns CSS into JS modules that inject <style> tags.
 					// In production, we use a plugin to extract that CSS to a file, but
 					// in development "style" loader enables hot editing of CSS.
+					{
+						test: /\.scss$/,
+						use: [
+							require.resolve('style-loader'),
+							{
+								loader: require.resolve('css-loader'),
+								options: {
+									importLoaders: 1,
+									modules: true,
+									localIdentName: '[name]__[local]___[hash:base64:5]',
+								},
+							},
+							{
+								loader: require.resolve('sass-loader'),
+							},
+							{
+								loader: require.resolve('postcss-loader'),
+								options: {
+									// Necessary for external CSS imports to work
+									// https://github.com/facebookincubator/create-react-app/issues/2677
+									ident: 'postcss',
+									plugins: () => [
+										require('postcss-flexbugs-fixes'),
+										autoprefixer({
+											browsers: [
+												'>1%',
+												'last 4 versions',
+												'Firefox ESR',
+												'not ie < 9', // React doesn't support IE8 anyway
+											],
+											flexbox: 'no-2009',
+										}),
+									],
+								},
+							},
+						],
+					},
 					{
 						test: /\.css$/,
 						use: [
--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import styles from './App.css';
+import './App.scss';
 
 class App extends Component {
 	render() {
